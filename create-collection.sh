36fb767145d788821301f835ef7c851aa507a195
Vincenzo D'Amore <v.damore@gmail.com>
2019-11-27 16:52:09 +0100

    change upload data

--- create-collection.sh
+++ create-collection.sh
@@ -2,3 +2,4 @@
 
 curl "localhost:8983/solr/admin/collections?action=CREATE&name=payloadtest&numShards=1&replicationFactor=1&maxShardsPerNode=1&collection.configName=payloadtest"
 curl -X POST "localhost:8983/solr/payloadtest/update" --data-binary @data.json -H 'Content-type:application/json'
+curl "http://localhost:8983/solr/payloadtest/update?commit=true"
